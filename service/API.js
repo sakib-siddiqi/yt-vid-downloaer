c0fa2da261fbd857c7ae6a266b444f4186e8178c
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2023-02-05 09:04:16 +0600

    production url change for api call

--- service/API.js
+++ service/API.js
@@ -1,7 +1,12 @@
+const DOMAIN =
+  process.env.NODE_ENV === "production"
+    ? "https://yt15.vercel.app/"
+    : "http://localhost:3000/";
+
 class APIClass {
-  root = "http://localhost:3000/api";
+  root = `${DOMAIN}api`;
   async search(q) {
-    return await (await fetch(this.root + "/search?yt="+q)).json();
+    return await (await fetch(this.root + "/search?yt=" + q)).json();
   }
 }
 
